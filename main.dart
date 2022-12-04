176f14cf8e4ac3388059b37de31297ecdb991d21
634601130007 <118664525+634601130007@users.noreply.github.com>
2022-12-04 14:24:03 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,53 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'report.dart';
+
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Shades(),
-    );
-  }
-}
-
-class Shades extends StatefulWidget {
-  const Shades({super.key});
-
-  @override
-  State<Shades> createState() => _ShadesState();
-}
-
-class _ShadesState extends State<Shades> {
-  int _shades = 468461;
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (details) {
-        setState(() {
-          _paint(context, details);
-        });
-      },
-      onVerticalDragUpdate: (details) {
-        setState(() {
-          _paint(context, details);
-        });
-      },
-      child: Scaffold(
-        backgroundColor: Color(0XFF000000 + _shades),
+    return MaterialApp(
+      theme: ThemeData(
+      textTheme: const TextTheme(
+        bodyText1: TextStyle(
+          fontSize: 20, 
+          color: Colors.white
+        ),
+        headline1: TextStyle(
+          fontSize: 72,
+          fontWeight: FontWeight.w900,
+          color: Colors.white,
+        ),
+        caption: TextStyle(
+          fontSize: 18,
+          fontStyle: FontStyle.italic,
+          color: Colors.yellow,
+        ),
+      )
+    ),
+      home: Scaffold(
+        body: Container(
+          constraints: const BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/cloud.gif'),
+              fit: BoxFit.cover
+            )
+          ),
+          child: const Report(),
+        ),
       ),
     );
   }
-  void _paint(context, details) {
-    double maxScr = MediaQuery.of(context).size.height;
-    double yPos = details.globalPosition.dy;
-    _shades = (yPos / maxScr * 16777215).round();
-    if(_shades > 16777215) _shades = 16777215;
-    //print(_shades);
-  }
 }
-
+ 
